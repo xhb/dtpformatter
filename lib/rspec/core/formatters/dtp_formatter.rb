436c51cbf2bd6822e0d00ed521f2c84a301afa94
xhb <programstart@163.com>
2014-05-19 17:45:06 +0800

    allp

--- lib/rspec/core/formatters/dtp_formatter.rb
+++ lib/rspec/core/formatters/dtp_formatter.rb
@@ -2,8 +2,12 @@ require 'time'
 
 class RSpec::Core::Formatters::Dtpformatter < RSpec::Core::Formatters::BaseFormatter
 
-  def xml
-    @xml ||= Builder::XmlMarkup.new :target => output, :indent => 2 
+  def report(key, value)
+    if output == STDOUT
+      output << "usercase: #{key}......time: #{value}\n"
+    else
+      system("%DTPDIR%\\bin\\report.exe --perf --table db_case_run --key #{key} --value #{value}")
+    end
   end
 
   def start example_count
@@ -13,39 +17,25 @@ class RSpec::Core::Formatters::Dtpformatter < RSpec::Core::Formatters::BaseForma
 
   def dump_summary duration, example_count, failure_count, pending_count
     super
-
-    xml.instruct!
-    xml.testsuite :tests => example_count, :failures => failure_count, :errors => 0, :time => '%.6f' % duration, :timestamp => @start.iso8601 do
-      xml.properties
-      examples.each do |example|
-        send :"dump_summary_example_#{example.execution_result[:status]}", example
-      end
+    output << "Tests: #{example_count}...Failures: #{failure_count}...Total_time:#{'%.6f' % duration}...Timestamp: #{@start.iso8601}\n"
+    examples.each do |example|
+      send :"dump_summary_example_#{example.execution_result[:status]}", example
     end
-  end
-
-  def xml_example example, &block
-    xml.testcase :classname => example_classname(example), :name => example.full_description, :time => '%.6f' % example.execution_result[:run_time], &block
   end
 
   def dump_summary_example_passed example
-    xml_example example
+    report(example.full_description, '%.6f' % example.execution_result[:run_time])
   end
 
   def dump_summary_example_pending example
-    xml_example example do
-      xml.skipped
-    end
+    report(example.full_description, "pending")
   end
 
   def dump_summary_example_failed example
     exception = example.execution_result[:exception]
     backtrace = format_backtrace exception.backtrace, example
-
-    xml_example example do
-      xml.failure :message => exception.to_s, :type => exception.class.name do
-        xml.cdata! "#{exception.message}\n#{backtrace.join "\n"}"
-      end
-    end
+    report(example.full_description, "-1")
+    output << "#{exception.message}\n#{backtrace.join "\n"}"
   end
 
   def example_classname example
